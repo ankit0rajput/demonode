d925698ba7fd2e2759b26be13affd08aa0f1f7f4
ankit0rajput <36675436+ankit0rajput@users.noreply.github.com>
2026-02-03 11:46:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js app 🚀');
 });
 
+app.get('/health', (req, res) => {
+  res.send('Health is ok');
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
